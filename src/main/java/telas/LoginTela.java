a3a095d7160056f3821ae23d9bc577dc38634153
Mateus Meneses <mateusmeneses86@gmail.com>
2022-11-09 15:26:11 -0300

    Initial commit

---
+++ src/main/java/telas/LoginTela.java
@@ -0,0 +1,11 @@
+package paginas;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginTela {
+    private WebDriver app;
+
+    public LoginTela(WebDriver app){
+        this.app = app;
+    }
+}
